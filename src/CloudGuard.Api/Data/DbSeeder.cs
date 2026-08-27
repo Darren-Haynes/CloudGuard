274a6e7029754f8b3921a711f71bd3911ea7e47c
Darren Haynes <darren.haynes.mr@outlook.com>
2026-08-26 23:49:45 -0500

    Merge pull request #4 from Darren-Haynes/feat/vulnerability-worker
    feat: implement asynchronous background vulnerability processing with boundary test automation

--- src/CloudGuard.Api/Data/DbSeeder.cs
+++ src/CloudGuard.Api/Data/DbSeeder.cs
@@ -1,4 +1,6 @@
 using CloudGuard.Api.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CloudGuard.Api.Data;
 
@@ -40,7 +42,64 @@ public static class DbSeeder
                 MissingPatches = 12,
                 SecurityStatus = "Critical",
                 LastAuditedAt = DateTime.UtcNow
-            });
+            },
+            new ServerAsset
+            {
+                ServerName = "gsy-mail-exch-01",
+                OperatingSystem = "Windows Server 2022",
+                MissingPatches = 0,
+                SecurityStatus = "Compliant",
+                LastAuditedAt = DateTime.UtcNow
+            },
+            new ServerAsset
+            {
+                ServerName = "jsy-legal-prod-01",
+                OperatingSystem = "Windows Server 2025",
+                MissingPatches = 1,
+                SecurityStatus = "Vulnerable",
+                LastAuditedAt = DateTime.UtcNow
+            },
+            new ServerAsset
+            {
+                ServerName = "jsy-fund-vm-03",
+                OperatingSystem = "Red Hat Enterprise Linux",
+                MissingPatches = 0,
+                SecurityStatus = "Compliant",
+                LastAuditedAt = DateTime.UtcNow
+            },
+            new ServerAsset
+            {
+                ServerName = "gsy-web-nginx-01",
+                OperatingSystem = "Ubuntu 24.04 LTS",
+                MissingPatches = 15,
+                SecurityStatus = "Critical",
+                LastAuditedAt = DateTime.UtcNow
+            },
+            new ServerAsset
+            {
+                ServerName = "gsy-sql-client-02",
+                OperatingSystem = "Windows Server 2022",
+                MissingPatches = 3,
+                SecurityStatus = "Vulnerable",
+                LastAuditedAt = DateTime.UtcNow
+            },
+            new ServerAsset
+            {
+                ServerName = "jsy-trust-prod-04",
+                OperatingSystem = "Windows Server 2019",
+                MissingPatches = 0,
+                SecurityStatus = "Compliant",
+                LastAuditedAt = DateTime.UtcNow
+            },
+            new ServerAsset
+            {
+                ServerName = "gsy-backup-nas-01",
+                OperatingSystem = "TrueNAS CORE",
+                MissingPatches = 7,
+                SecurityStatus = "Vulnerable",
+                LastAuditedAt = DateTime.UtcNow
+            }
+        );
 
         context.SaveChanges();
     }
